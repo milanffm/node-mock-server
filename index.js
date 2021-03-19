054243dfe086d5fcc26c46922e58601e5c0e92c2
Milan Peters <milan.peters@vrm.de>
2021-03-19 16:46:59 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 4123
+
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+
+server.get('/api/users', (request, response) => {
+if (request.method === 'GET') {
+        const users = require('./api/users')
+        response.status(200).jsonp(users())
+    }
+})
+
+server.listen(port, () => {
+    console.log('JSON Server is running')
+})
